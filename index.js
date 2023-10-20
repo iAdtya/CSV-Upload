0c4e5cf545b10b06d529a2b254b8a8befeff6454
Aditya Khedekar <adityakhedekar98906@gmail.com>
2023-10-20 22:13:05 +0530

    done 1

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const port = process.env.port || 8001;
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("./assets"));
