e184464a2dacb2296d6d61553a5ea94e00d21b33
Aditya Khedekar <adityakhedekar98906@gmail.com>
2023-10-21 03:25:55 +0530

    dasd

--- controllers/upload_controller.js
+++ controllers/upload_controller.js
@@ -4,18 +4,18 @@ import path from "path";
 import parse from "csv-parser";
 import fs from "fs";
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/files");
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(
-      null,
-      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
-    );
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "uploads/files");
+//   },
+//   filename: (req, file, cb) => {
+//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+//     cb(
+//       null,
+//       file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+//     );
+//   },
+// });
 
 export const upload = multer({ storage: storage });
 
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import multer from "multer";  
 import home from "../controllers/home_controller.js";
 
 import {
